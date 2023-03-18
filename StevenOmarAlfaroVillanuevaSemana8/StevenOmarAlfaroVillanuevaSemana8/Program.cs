b4fb8d50eccc0434a0034004ba0ed5c3939aa6c7
G3R50NP11 <125324495+Owar824@users.noreply.github.com>
2023-03-18 07:13:50 +0100

    proyecto

---
+++ StevenOmarAlfaroVillanuevaSemana8/StevenOmarAlfaroVillanuevaSemana8/Program.cs
@@ -0,0 +1,26 @@
+﻿using StevenOmarAlfaroVillanuevaSemana8.Models;
+
+using AlmacenContext db = new AlmacenContext();
+
+Producto producto = new Producto();
+
+Console.WriteLine("Ingrese el nombre del producto: ");
+producto.Nombre = Console.ReadLine();
+
+Console.WriteLine("Descripcion del Producto: ");
+producto.Descripcion = Console.ReadLine();
+
+Console.WriteLine("Ingrese el Precio del producto : ");
+producto.Precio = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad de producto disponible: ");
+producto.Stock = Convert.ToInt32(Console.ReadLine());
+db.Productos.Add(producto);
+db.SaveChanges();
+
+var ListProducto = db.Productos.ToList();
+
+foreach (var produc in ListProducto) 
+{
+    Console.WriteLine(produc.Nombre);
+}
